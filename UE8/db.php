386dcf8d0ac188a8d3ae7eb880e892c775ece554
Peter K <p.kurz@htlkrems.at>
2017-02-14 11:24:28 +0100

    Create db.php

---
+++ UE8/db.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<title></title>
+	</head>
+	<body>
+		<div class="container">
+			<?php
+				$host = 'localhost';
+				$dbname = 'medt3';
+				$user = 'htluser';
+				$pwd = 'htluser';
+				
+				$db = new PDO ( "mysql:host=$host;dbname=$dbname", $user, $pwd);
+				if ($db == true)
+					echo "true";
+				else
+					echo "false";
+			?>
+		</div>
+	</body>
+</html>
